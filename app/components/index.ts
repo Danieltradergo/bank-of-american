cc88481dea95d64d2a2bf4a7748017afa9af71d9
Danieltradergo <danieltradergo@icloud.com>
2025-12-24 22:12:42 -0300

    Create index.ts

---
+++ app/components/index.ts
@@ -0,0 +1,8 @@
+// Component exports for Bank of American
+export { default as Footer } from './footer';
+export { default as DashboardSection } from './dashboard';
+export { default as SiteHeader } from './site-header';
+
+// Types and Utilities exports
+export * from './types';
+export * from './utils';
